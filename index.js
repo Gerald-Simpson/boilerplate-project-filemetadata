9c35b334b3df6209bd82088897ca692d6b303ff4
Gerald-Simpson <gtnsimpson@gmail.com>
2022-11-28 16:57:12 +0000

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 var app = express();
 
@@ -11,10 +11,7 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
