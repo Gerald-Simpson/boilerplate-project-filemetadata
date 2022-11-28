8fc7cd726779037b05ae33f5327707039ac47377
Gerald-Simpson <gtnsimpson@gmail.com>
2022-11-28 17:30:08 +0000

    Complete project test

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var multer = require('multer');
 var cors = require('cors');
 require('dotenv').config();
+const upload = multer({ dest: './public/uploads/' });
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
 });
 
 const port = process.env.PORT || 3000;
